76ba8670f6baa688a75fcfede65c36d6f2af217d
prabakaran <mspk028@gmail.com>
2023-03-11 10:22:58 +0530

    components added

--- src/App.js
+++ src/App.js
@@ -1,13 +1,79 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "./styles/App.css";
 import "./styles/normalize.css";
-// import { FaEdit } from "react-icons/fa";
+import Weather from "./components/Weather";
+import { FaCloudSunRain, FaSearch } from "react-icons/fa";
 
 const App = () => {
+  const [value, setValue] = useState("");
+  const [data, setData] = useState([]);
+
+  let url = `http://api.weatherapi.com/v1/current.json?key=74cfee985da04864bd390458230802&q=${value}`;
+
+  const getWeather = async () => {
+    await fetch(url)
+      .then((response) => {
+        // if (!response.ok) {
+        //   alert("No weather found !!!");
+        //   throw new Error("No weather found !!!");
+        // }
+        return response.json();
+      })
+      .then((data) => setData([data]));
+  };
+
+  useEffect(() => {
+    getWeather();
+  }, []);
+
   return (
-    <div>
-      <h1>Hello</h1>
-    </div>
+    <>
+      {/* input container */}
+      <div className="input-container">
+        <h2 className="title">
+          Weather <span>APP </span> <FaCloudSunRain />
+        </h2>
+      </div>
+
+      {/*  input */}
+      <div className="btn-cont">
+        <input
+          type="text"
+          name="city"
+          placeholder="Enter a city name..."
+          value={value}
+          onChange={(e) => setValue(e.target.value)}
+          autoComplete="off"
+        />
+        <div className="search-icon">
+          <FaSearch />
+        </div>
+
+        {/* btn animation */}
+        <button type="button" onClick={getWeather}>
+          <div className="svg-wrapper-1">
+            <div className="svg-wrapper">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 24 24"
+                width="24"
+                height="24"
+              >
+                <path fill="none" d="M0 0h24v24H0z"></path>
+                <path
+                  fill="currentColor"
+                  d="M1.946 9.315c-.522-.174-.527-.455.01-.634l19.087-6.362c.529-.176.832.12.684.638l-5.454 19.086c-.15.529-.455.547-.679.045L12 14l6-8-8 6-8.054-2.685z"
+                ></path>
+              </svg>
+            </div>
+          </div>
+          <span>Search</span>
+        </button>
+      </div>
+
+      {/* output container */}
+      <Weather data={data} />
+    </>
   );
 };
 
--- src/components/Weather.js
+++ src/components/Weather.js
@@ -1,7 +1,52 @@
 import React from "react";
 
-const Weather = () => {
-  return <div>Weather</div>;
+const Weather = ({ data }) => {
+  function getTime(timeNow) {
+    // to get time & date in string format
+    let d = new Date(timeNow.location.localtime);
+    let weekday = d.toLocaleString("default", { weekday: "short" });
+    let month = d.toLocaleString("default", { month: "short" });
+    let date = d.getDate();
+    let hour = d.getHours();
+    let mins = d.getMinutes();
+
+    let period = "AM";
+
+    let h = hour > 12 ? hour - 12 : hour;
+    let hr = h < 10 ? `0${h}` : h;
+    let m = mins < 10 ? `0${mins}` : mins;
+    period = hour >= 12 ? "PM" : "AM";
+
+    return `${weekday}, ${month} ${date} - ${hr} : ${m} ${period}`;
+  }
+
+  // let getTime=
+
+  return (
+    <>
+      {data.map((weather, index) => {
+        // console.log(weather);
+        return (
+          <div className="output-container" key={index}>
+            <h2> {weather.location.name}</h2>
+            <p className="country">{weather.location.country}</p>
+            <p className="region">{weather.location.region}</p>
+            <p className="date">{getTime(weather)}</p>
+            <h1 className="celcius">{weather.current.temp_c}°C</h1>
+            <div className="icon-contaoner">
+              <p className="type">{weather.current.condition.text}</p>
+              <div className="weather-icons">
+                <img src={""} className="weather-icon" alt="icon" />
+              </div>
+            </div>
+            <p className="range">
+              {weather.current.feelslike_c}°C / ${weather.current.temp_c}°C
+            </p>
+          </div>
+        );
+      })}
+    </>
+  );
 };
 
 export default Weather;
