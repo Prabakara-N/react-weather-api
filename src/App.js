8e46facef8e05736ba1c7b6de4fbd865f7e0d7f1
prabakaran <mspk028@gmail.com>
2023-03-11 08:48:06 +0530

    Weather api project setup

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./styles/App.css";
+import "./styles/normalize.css";
+// import { FaEdit } from "react-icons/fa";
+
+const App = () => {
+  return (
+    <div>
+      <h1>Hello</h1>
+    </div>
+  );
+};
+
+export default App;
